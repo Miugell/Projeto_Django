8edcde3094e18e49a0609f97f5499bcdec5bd1fe
MIGUEL DE MENDONCA SILVA <miguel.msilva10@senacsp.edu.br>
2024-04-16 21:52:48 -0300

    att faker

--- entertainments/views.py
+++ entertainments/views.py
@@ -1,14 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from utils.entertainments.factore import make_post
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'entertainments/pages/home.html', context={
-        'name' : 'Miguel',
+        'posts' : [make_post() for _ in range(10)],
     })
 
 
 def post(request, id):
-    return render(request, 'entertainments/pages/post-view.html')
+    return render(request, 'entertainments/pages/post-view.html', context={
+        'post' : make_post,
+    })
